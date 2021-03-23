5a9798396d49a9af70896732fb76122720dc6655
Supervodka <supervodka666@gmail.com>
2021-03-23 18:08:17 +0300

    Добавьте файлы проекта.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press smth");
+            string selection= Console.ReadLine();
+            switch (selection)
+            {
+                case "Boris":
+                    Console.WriteLine("Blade");
+                    break;
+                case "Tone":
+                    Console.WriteLine("Bullet Dodger");
+                    break;                   
+                case "Avi":
+                    Console.WriteLine("Jew");
+                    break;
+                default:
+                    Console.WriteLine("Snatch");
+                    break;
+            }
+        }
+    }
+}
